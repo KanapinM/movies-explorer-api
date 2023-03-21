96e167e8fd8bb87505c8f64a143c155814b422fe
Kanapin Marat <kanapinmarat@yandex.ru>
2023-03-21 20:04:54 +0300

    refactor: fix cors for local dev

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,7 +1,7 @@
 const allowedCors = [
   'https://kino.nomoredomains.work',
   'http://kino.nomoredomains.work',
-  'localhost:3000',
+  'http://localhost:3000',
 ];
 
 const cors = function (req, res, next) {
